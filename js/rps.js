6a24325ac908c63417476f04e010ad0e2ab6e3d1
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-04-29 22:19:36 +0800

    create constant variable for possible values

--- js/rps.js
+++ js/rps.js
@@ -1,3 +1,5 @@
+const VALUES = ['rock', 'paper', 'scissor'];
+
 /* will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. 
     We’ll use this function in the game to make the computer’s play */
 function getComputerChoice (){
@@ -8,15 +10,20 @@ function getComputerChoice (){
     // depending on the number return
     switch (num) {
         case 1:
-            return 'Rock';
-            break;
+            return VALUES[num-1];
         case 2:
-            return 'Paper';
-            break;
+            return VALUES[num-1];
         case 3:
-            return 'Scissors'
-            break;
+            return VALUES[num-1];
     }
 }
 
 console.log(getComputerChoice());
+
+
+//function that plays a single round of Rock Paper Scissors
+//return who's the winner
+const playRound = (playerSelection, computerSelection) => {
+    //compare players choice
+    
+}
