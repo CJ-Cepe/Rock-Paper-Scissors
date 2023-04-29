6ef74b7d0529973182d20556ca9a5b448d6fbc43
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-04-29 19:23:06 +0800

    create function that returns Rock Paper Scissors

--- js/rps.js
+++ js/rps.js
@@ -1,3 +1,22 @@
+/* will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. 
+    We’ll use this function in the game to make the computer’s play */
 function getComputerChoice (){
+    // pick random num between [1-3]
+    let num = Math.floor(Math.random() * 3) + 1;
+    console.log(num);
     
+    // depending on the number return
+    switch (num) {
+        case 1:
+            return 'Rock';
+            break;
+        case 2:
+            return 'Paper';
+            break;
+        case 3:
+            return 'Scissors'
+            break;
+    }
 }
+
+console.log(getComputerChoice());
