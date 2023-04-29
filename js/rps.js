745d0e33e56fffb80417641440c1e891a264e85d
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-04-29 23:10:11 +0800

    create function to play the game

--- js/rps.js
+++ js/rps.js
@@ -1,40 +1,57 @@
+
+
 const VALUES = ['rock', 'paper', 'scissor'];
 
 /* will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. 
-    We’ll use this function in the game to make the computer’s play */
+   We’ll use this function in the game to make the computer’s play */
 function getComputerChoice (){
     // pick random num between [1-3]
     let num = Math.floor(Math.random() * 3) + 1;
-    console.log(num);
+    //console.log(num);
     
     // depending on the number return
     switch (num) {
         case 1:
-            return VALUES[num-1];
+            return num-1;
         case 2:
-            return VALUES[num-1];
+            return num-1;
         case 3:
-            return VALUES[num-1];
+            return num-1;
     }
 }
-
-console.log(getComputerChoice());
 
 
 //function that plays a single round of Rock Paper Scissors
 //return who's the winner
 const playRound = (playerSelection, computerSelection) => {
+    console.log(VALUES[playerSelection] + " vs " + VALUES[computerSelection])
     //compare players choice
     if(playerSelection === computerSelection){
         return "It's a tie";
     } else if (playerSelection === VALUES.length-1 && computerSelection === 0) {
-        return "Computer wins"
+        return "Computer wins";
     } else if (playerSelection === 0 && computerSelection === VALUES.length-1) {
-        return "Player wins"
+        return "Player wins";
     } else if (playerSelection < computerSelection) {
-        return "Computer wins"
+        return "Computer wins";
     } else {
-        return "Player wins"
+        return "Player wins";
     }
+}
+
+const game = function () {
+    //ask player num
+    let ans = prompt("Pick between Rock, Paper, and Scissors");
+    //lowercase prompt and check if in array
+    console.log(ans.toLowerCase())
+    let playerAns = VALUES.indexOf(ans.toLowerCase());
+    console.log(playerAns);
+    let computerAns = getComputerChoice();
+    let result = playRound(playerAns, computerAns);
+    console.log(result)
+}
+
+for(i = 0; i < 5 ; i++){
+    game();
 }
 
