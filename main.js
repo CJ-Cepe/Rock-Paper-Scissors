5556bf557a46616b1faa9edd61ac8da3e3aed1fa
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-05-20 22:22:56 +0800

    remove unnecesary lines

--- main.js
+++ main.js
@@ -34,21 +34,9 @@ const elements = {
     playButtonElem: document.querySelector('.play-button'),
 }
 
-const audio = {
-    battle: new Audio('../assets/audio/battle.mp3'),
-    loss: new Audio('../assets/audio/loss.mp3'),
-    victory: new Audio('../assets/audio/victory.mp3'),
-    damage: new Audio('../assets/audio/damage.mp3'),
-    absorb: new Audio('../assets/audio/absorb.mp3'),
-}
-
 let round = 0;
 let playerScore = 0;
 let cpuScore = 0;
-
-let githubAddress = 'https://github.com/CJ-Cepe/Rock-Paper-Scissors/raw/main/assets/audio/'
-githubAddress = './assets/audio/'
-let githubAddressEnd = '' //?raw=true
 
 elements.pokeballOne.addEventListener('click', () => {
     resetPokemonColor()
@@ -76,7 +64,7 @@ elements.resetButtonElem.addEventListener('click', () => {
 
 elements.playButtonElem.addEventListener('click', function () {
     playAudio(1)
-    //audio.battle.play()
+   
     elements.introModalElem.classList.add('hidden')
     elements.overlayElem.classList.add('hidden')
     elements.overlayElem.style.background = 'background: rgba(0, 0, 0, 0.5)'
@@ -93,22 +81,22 @@ playGame()
 function playAudio(whatAudio){
     switch (whatAudio) {
         case 1:
-            elements.audioElem.src = githubAddress + 'battle.mp3' + githubAddressEnd
+            elements.audioElem.src = './assets/audio/battle.mp3' 
             elements.audioElem.play()
             break;
         case 2:
-            elements.audioElem.src = githubAddress + 'victory.mp3' + githubAddressEnd
+            elements.audioElem.src = './assets/audio/victory.mp3'
             elements.audioElem.play()
             break;
         case 3:
-            elements.audioElem.src = githubAddress + 'loss.mp3' + githubAddressEnd
+            elements.audioElem.src = './assets/audio/loss.mp3'
             elements.audioElem.play()
         case 4:
-            elements.audio2Elem.src = githubAddress + 'damage.mp3' + githubAddressEnd
+            elements.audio2Elem.src = './assets/audio/damage.mp3'
             elements.audio2Elem.play()
             break;
         case 5:
-            elements.audio2Elem.src = githubAddress + 'absorb.mp3' + githubAddressEnd
+            elements.audio2Elem.src = './assets/audio/absorb.mp3'
             elements.audio2Elem.play()
             break;
         case 6:
@@ -229,21 +217,15 @@ function updateResult(roundResult){
     switch(roundResult){
         case 0:
             elements.cont1Elem.style.backgroundColor = '#273c75';
-            //elements.leftElem.style.backgroundColor = 'transparent';
-            //elements.rightElem.style.backgroundColor = 'transparent';
             elements.roundResultElem.textContent = `It's a Tie`;
-            //audio.absorb.play()
             playAudio(5)
             break;
         case 1:
             playerScore++;
             elements.cont1Elem.style.backgroundColor = '#44bd32';
-            //elements.leftElem.style.backgroundColor = 'green';
-            //elements.rightElem.style.backgroundColor = 'transparent';
             elements.roundResultElem.textContent = `Player wins this round`;
             elements.pokemonRightElem.style.filter = 'grayscale(100%)'
             elements.pokemonRightElem.classList.add('blink')
-            //audio.damage.play()
             playAudio(4)
             setTimeout(() => {
                 elements.pokemonRightElem.classList.remove('blink')
@@ -252,12 +234,9 @@ function updateResult(roundResult){
         case 2: 
             cpuScore++;
             elements.cont1Elem.style.backgroundColor = '#c23616';
-            //elements.rightElem.style.backgroundColor = 'green';
-            //elements.leftElem.style.backgroundColor = 'transparent';
             elements.roundResultElem.textContent = `CPU wins this round`;
             elements.pokemonLeftElem.style.filter = 'grayscale(100%)'
             elements.pokemonLeftElem.classList.add('blink')
-            //audio.damage.play()
             playAudio(4)
             setTimeout(() => {
                 elements.pokemonLeftElem.classList.remove('blink')
@@ -297,13 +276,11 @@ function displayWinner(winner){
     
     if(winner){
         playAudio(2);
-        //elements.modalElem.style.color = 'green'
         elements.modalLeftElem.style.color = `#44bd32`
         elements.modalTextElem.textContent = `Player`
         elements.modalTrainerElem.src = elements.trainerLeftElem.src
     } else {
         playAudio(3);
-        //elements.modalElem.style.backgroundColor = 'red';
         elements.modalLeftElem.style.color = `#e84118`
         elements.modalTextElem.textContent = `CPU`
         elements.modalTrainerElem.src = elements.trainerRightElem.src;
@@ -335,8 +312,6 @@ function reset(){
     elements.roundElem.textContent = ``
 
     elements.cont1Elem.style.backgroundColor = 'transparent';
-    //elements.leftElem.style.backgroundColor = 'transparent';
-    //elements.rightElem.style.backgroundColor = 'transparent';
 
     elements.pokemonLeftElem.src = ''
     elements.pokemonRightElem.src = ''
